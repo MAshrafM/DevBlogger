5b9f68f67630f8a4d3a58f34115a75d4dab75c78
MAshraf <ee.mashraf@gmail.com>
2018-08-05 17:59:27 +0300

    nav stracture

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,42 @@
 </head>
 <body>
     <div id="app">
-    
+    <nav class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item" href="{{route('home')}}">
+            <img src="{{ asset('images/logo.png')}}" alt="logo" />
+          </a>
+          <a class="nav-item is-tab is-hidden-mobile m-l-10">
+          Learn
+          </a>
+          <a class="nav-item is-tab is-hidden-mobile">
+          Discuss
+          </a>
+          <a class="nav-item is-tab is-hidden-mobile">
+          Share
+          </a>
+        </div>
+        <div class="nav-right">
+          @guest
+            <a href="#" class="nav-item is-tab">Login</a>
+            <a href="#" class="nav-item is-tab">Join Us</a>
+          @end
+          @auth
+            <button class="dropdown nav-item is-tab">
+              Hi, User <span class="icon"><i class="fa fa-caret-down"></i></span>
+              <ul class="dropdown-menu">
+                <li><a href="">Profile</a></li>
+                <li><a href="">Notification</a></li>
+                <li><a href="">Settings</a></li>
+                <li class="seprator"></li>
+                <li><a href="">Logout</a></li>
+              </ul>
+            </button>
+          @end
+        </div>
+      </div>
+    </nav>
       <main class="py-4">
           @yield('content')
       </main>

92149a9cfa56c79de14a689e53c35124feb7629b
MAshraf <ee.mashraf@gmail.com>
2018-08-08 13:08:13 +0300

    refactor

---
+++ resources/views/_includes/nav/main.blade.php
@@ -0,0 +1,47 @@
+<nav class="navbar has-shadow">
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="{{route('home')}}">
+        <img src="{{ asset('images/logo.png')}}" alt="logo" />
+      </a>
+      
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item is-tab m-l-10">
+        Learn
+        </a>
+        <a class="navbar-item is-tab">
+        Discuss
+        </a>
+        <a class="navbar-item is-tab">
+        Share
+        </a>
+      </div>
+      <div class="navbar-end">
+        @guest
+          <a class="navbar-item is-tab" href="{{ route('login')}}"> Login </a>
+          <a class="navbar-item is-tab" href="{{ route('register')}}"> Join Us </a>
+        @endguest
+        @auth
+          <div class="dropdown navbar-item is-tab has-dropdown is-aligned-right is-hoverable">
+            <a class="navbar-link">Hi, {{ Auth::user()->name }}</a>
+            <div class="dropdown-menu navbar-dropdown">
+              <a class="navbar-item" href=""><span class="icon"><i class="fa fa-fw m-r-10 fa-user-circle-o"></i></span> Profile</a>
+              <a class="navbar-item" href=""><span class="icon"><i class="fa fa-fw m-r-10 fa-bell"></i></span> Notification</a>
+              <a class="navbar-item" href=""><span class="icon"><i class="fa fa-fw m-r-10 fa-cog"></i></span> Settings</a>
+              <hr class="navbar-divider">
+              <a class="navbar-item" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><span class="icon"><i class="fa fa-fw m-r-10 fa-sign-out"></i></span> Logout</a>
+              @include('_includes.forms.logout')
+            </div>
+          </div>
+        @endauth
+      </div>
+    </div>
+  </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,68 +9,24 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>DevBlogger</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
-    <div id="app">
-    <nav class="navbar has-shadow">
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="{{route('home')}}">
-            <img src="{{ asset('images/logo.png')}}" alt="logo" />
-          </a>
-          
-          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-        <div class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item is-tab m-l-10">
-            Learn
-            </a>
-            <a class="navbar-item is-tab">
-            Discuss
-            </a>
-            <a class="navbar-item is-tab">
-            Share
-            </a>
-          </div>
-          <div class="navbar-end">
-            @guest
-              <a class="navbar-item is-tab" href="{{ route('login')}}"> Login </a>
-              <a class="navbar-item is-tab" href="{{ route('register')}}"> Join Us </a>
-            @endguest
-            @auth
-              <div class="dropdown navbar-item is-tab has-dropdown is-aligned-right is-hoverable">
-                <a class="navbar-link">Hi, User</a>
-                <div class="dropdown-menu navbar-dropdown">
-                  <a class="navbar-item" href=""><span class="icon"><i class="fa fa-fw m-r-10 fa-user-circle-o"></i></span> Profile</a>
-                  <a class="navbar-item" href=""><span class="icon"><i class="fa fa-fw m-r-10 fa-bell"></i></span> Notification</a>
-                  <a class="navbar-item" href=""><span class="icon"><i class="fa fa-fw m-r-10 fa-cog"></i></span> Settings</a>
-                  <hr class="navbar-divider">
-                  <a class="navbar-item" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><span class="icon"><i class="fa fa-fw m-r-10 fa-sign-out"></i></span> Logout</a>
-                </div>
-              </div>
-            @endauth
-          </div>
-        </div>
-      </div>
-    </nav>
-      <main class="py-4">
-          @yield('content')
-      </main>
-    </div>
+  @include('_includes.nav.main')
+  <div id="app">
+    <main class="py-4">
+        @yield('content')
+    </main>
+  </div>
+  
+  <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    @yield('scripts')
 </body>
 </html>

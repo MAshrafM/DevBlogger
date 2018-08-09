c3fd2e122e1eeedcd5e4562b7a099442ed309fc0
MAshraf <ee.mashraf@gmail.com>
2018-08-09 16:18:52 +0300

    roles edit update

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -81,7 +81,9 @@ class RoleController extends Controller
      */
     public function edit($id)
     {
-        //
+      $role = Role::where('id', $id)->with('permissions')->first();
+      $permissions = Permission::all();
+      return view('manage.roles.edit')->withRole($role)->withPermissions($permissions);
     }
 
     /**
@@ -93,7 +95,23 @@ class RoleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $this->validate($request, [
+        'display_name' => 'required|max:255',
+        'description' => 'sometimes|max:255'
+      ]);
+      
+      $role = Role::findOrFail($id);
+      $role->display_name = $request->display_name;
+      $role->description = $request->description;
+      
+      $role->save();
+      
+      if($request->permissions){
+        $role->syncPermissions(explode(',', $request->permissions));
+      }
+      
+      Session::flash('success', 'Role updated successfully');
+      return redirect()->route('roles.show', $role->id);
     }
 
     /**
